f16ad9f5afff46a9c29b1401796212b12c4608a4
JakKowalCzyk <beufort@o2.pl>
2016-09-29 20:16:41 +0200

    restaurant entity, foodtype enum added

---
+++ src/main/java/com/kowalczyk/exercise/FoodType.java
@@ -0,0 +1,7 @@
+package com.kowalczyk.exercise;
+
+/**
+ * Created by bofort on 29.09.16.
+ */
+public enum FoodType {
+}
---
+++ src/main/java/com/kowalczyk/exercise/entity/Restaurant.java
@@ -0,0 +1,7 @@
+package com.kowalczyk.exercise.entity;
+
+/**
+ * Created by bofort on 29.09.16.
+ */
+public class Restaurant {
+}
